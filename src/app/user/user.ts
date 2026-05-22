fb77f9bf112bc24d461e98adf46145563692636e
Tejeswar Reddy <btejeswarreddy01@gmail.com>
2026-05-22 11:30:01 -0400

    second commit - until sec2-28

---
+++ src/app/user/user.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users'
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+ selectedUser = DUMMY_USERS[randomIndex];
+
+ get ImagePath()
+ {
+   return './assets/users/' + this.selectedUser.avatar;
+ }
+
+ onSelectUser()
+ {
+   console.log("Clicked!");
+   const randomIndex1 = Math.floor(Math.random() * DUMMY_USERS.length)
+   this.selectedUser = DUMMY_USERS[randomIndex1];
+ }
+}
