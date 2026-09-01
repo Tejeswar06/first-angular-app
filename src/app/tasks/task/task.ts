7d22c4ab8add1603c1ad9ef462d7dd71ebff394a
Tejeswar Reddy <btejeswarreddy01@gmail.com>
2026-09-01 10:49:49 -0400

    Outputting user specific task and task data in task component

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface task{
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   imports: [],
@@ -7,5 +14,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.css',
 })
 export class Task {
-
+  @Input({required: true}) task! : task;
 }
